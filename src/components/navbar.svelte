3bb7d2b10b47f654bc866e97ba446f8ed83982f8
Frail Bongat <frail.bongat@gmail.com>
2022-08-22 22:55:30 +0800

    style: use green shades for active styles

--- src/components/navbar.svelte
+++ src/components/navbar.svelte
@@ -17,10 +17,10 @@
 		{#if !searched}
 			<ul class="menu menu-horizontal p-0 hidden md:flex">
 				<li>
-					<a href="/" class={$page.url.pathname === '/' ? 'text-primary' : ''}>Apps</a>
+					<a href="/" class={$page.url.pathname === '/' ? 'text-green-600' : ''}>Apps</a>
 				</li>
 				<li>
-					<a href="/games" class={$page.url.pathname === '/games' ? 'text-primary' : ''}>Games</a>
+					<a href="/games" class={$page.url.pathname === '/games' ? 'text-green-500' : ''}>Games</a>
 				</li>
 			</ul>
 		{/if}
